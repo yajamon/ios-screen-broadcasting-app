bf420f4f02cc30f23358f9d9f8b849017131f8af
yajamon <yajamon.tatsuki@gmail.com>
2020-06-21 02:59:47 +0900

    xcodegenによってプロジェクトを生成できるようにした

---
+++ bin/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+readonly DIRPATH=$(cd $(dirname ${BASH_SOURCE:-$0})/; pwd)
+readonly REPO_ROOT=$(cd $DIRPATH/..; pwd)
+readonly PROJECT_NAME='ScreenBroadcasting'
+
+if ! test -d $REPO_ROOT/$PROJECT_NAME; then
+    echo "Missing: $REPO_ROOT/$PROJECT_NAME" >&2
+    exit 1
+fi
+
+cd $REPO_ROOT/$PROJECT_NAME
+xcodegen generate
