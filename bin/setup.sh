691983b7be2232cb5b0ad11e04e241b077b75b79
yajamon <yajamon.tatsuki@gmail.com>
2020-06-22 01:24:52 +0900

    Setupスクリプトにてxcodegenを要求する

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,6 +4,11 @@ readonly DIRPATH=$(cd $(dirname ${BASH_SOURCE:-$0})/; pwd)
 readonly REPO_ROOT=$(cd $DIRPATH/..; pwd)
 readonly PROJECT_NAME='ScreenBroadcasting'
 
+if ! type -a xcodegen; then
+    echo "Missing command: xcodegen" >&2
+    exit 1
+fi
+
 if ! test -d $REPO_ROOT/$PROJECT_NAME; then
     echo "Missing: $REPO_ROOT/$PROJECT_NAME" >&2
     exit 1
