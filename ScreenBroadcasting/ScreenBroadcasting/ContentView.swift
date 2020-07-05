89783151c2fdb5a49224365fca5d7ca0a25c8922
yajamon <yajamon.tatsuki@gmail.com>
2020-07-05 12:45:23 +0900

    とりあえずボタン類を配置してみる

--- ScreenBroadcasting/ScreenBroadcasting/ContentView.swift
+++ ScreenBroadcasting/ScreenBroadcasting/ContentView.swift
@@ -10,7 +10,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+            Spacer()
+            HStack {
+                Button("Recording") {
+                }
+                Spacer()
+                Button("RecStop") {
+                }
+            }
+        }.padding()
     }
 }
 
